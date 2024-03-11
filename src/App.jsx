966ce63ce7292ed39346402b8bcaa46ad337c7de
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-03-11 12:49:55 +0500

    conversion for links

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {Helmet} from "react-helmet";
+import './assets/frontend/assets/bootstrap/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <>
-     <h1>Usama Hassan</h1> 
+    <Helmet>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    </Helmet>
+     <h1 className='bg-light'>Usama Hassan</h1> 
     </>
   )
 }
