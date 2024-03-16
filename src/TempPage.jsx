619e65057ff97a562334ebd738af0844b0657305
Usama Hassan <ahmarkhan1233456@gmail.com>
2024-03-16 14:07:47 +0500

    Single Blog

---
+++ src/TempPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TempPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default TempPage
